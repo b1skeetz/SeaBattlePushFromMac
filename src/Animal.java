42e271b7200cafd9b0de39c697c6be080c63036d
Admin <admin@MacBook-Pro-Admin.local>
2023-05-23 17:53:34 +0600

    new method

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Animal {
+public class Animal {
+    private String name;
+    private int age;
+
+    public Animal(){
+
+    }
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void voice(){
+        System.out.println("Cat is meowing");
+    }
 }
